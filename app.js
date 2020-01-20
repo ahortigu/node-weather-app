65cff2e2ae1723f57b0b88dc28db5a002f81e59c
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-20 15:41:01 +0000

    Object Destructuring included

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ const address = process.argv[2]
     if (!address){
         console.log ('Please provide a location')
     } else{
-        geocode(address, (error, data) => {
+        //geocode(address, (error, data), here I am destructuring data.
+        geocode(address, (error, {latitude, longitude, location}) => {
             if(error){
                 return console.log('Error', error)
             }
-            forecast(data.latitude, data.longitude, (error, forecastData) => {
+            // forecast(data.latitude, data.longitude, (error, forecastData) => {
+            forecast(latitude,longitude, (error, forecastData) => {
                 if(error){
                     return console.log(error)
                 }
-                console.log(data.location)
+                //console.log(data.location)
+                console.log(location)
+                //console.log(data.forecastData)
                 console.log(forecastData)
             })
         })
--- utils/forecast.js
+++ utils/forecast.js
@@ -15,13 +15,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback)=>{
     const url = 'https://api.darksky.net/forecast/ce6678b032480b1a6eebc71de3e5e0cd/' + latitude + ',' + longitude
 
-    request({url: url, json:true}, (error, response) =>{ 
+    request({url, json:true}, (error, {body}) =>{ 
         if(error){
             callback('Unable to connect to weather server', undefined)
-        } else if (response.body.error){
+        } else if (body.error){
             callback('Unable to find location to check the weather.', undefined)
         } else {
-           callback (undefined, response.body.daily.data[0].summary +'It is currently ' + response.body.currently.temperature +  ' degrees out. There is ' + response.body.currently.precipProbability + ' % chance of rain.')
+           callback (undefined, body.daily.data[0].summary +'It is currently ' + body.currently.temperature +  ' degrees out. There is ' + body.currently.precipProbability + ' % chance of rain.')
         }
 })
 }
